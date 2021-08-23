b5f8cbf0f8ebc0c095bbfcad17784926ea7a6f4e
PaoloBettelini <paolo.bettelini@samtrevano.ch>
2021-08-23 14:05:17 +0200

    byte utils

---
+++ byteutils/Offset.java
@@ -0,0 +1,41 @@
+package com.thecrownstudios.relay.common;
+
+public class Offset {
+
+    private int value;
+
+    public Offset() {
+        this.value = 0;
+    }
+
+    public Offset(int value) {
+        this.value = value;
+    }
+
+    public int get() {
+        return value;
+    }
+
+    public void set(int value) {
+        this.value = value;
+    }
+
+    public int addAndGet(int value) {
+        return this.value += value;
+    }
+
+    public int getAndAdd(int value) {
+        int v = this.value;
+        this.value += value;
+        return v;
+    }
+
+    public int getAndIncrement() {
+        return value++;
+    }
+
+    public int incrementAndGet() {
+        return ++value;
+    }
+
+}
--- tablegenerator/Table.java
+++ tablegenerator/Table.java
@@ -1,3 +1,5 @@
+package tablegenerator;
+
 import java.util.LinkedList;
 import java.util.List;
 
